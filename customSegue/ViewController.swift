6eadd2105a3b17d44f54aaa0f5cc09b0fcbe042d
dev <bene.a99@gmail.com>
2019-04-12 07:00:18 +0200

    Initial Commit

---
+++ customSegue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  customSegue
+//
+//  Created by Benjamin on 2019-04-12.
+//  Copyright © 2019 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
